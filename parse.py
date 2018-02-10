48982a7d2a6948de5dd0cfcec70a3d91563a4eeb
Boyko Perfanov <b_perfanov@yahoo.com>
2018-02-10 08:43:24 -0500

    Parametrize parse.py for dir and max number of files.

--- parse.py
+++ parse.py
@@ -1,15 +1,13 @@
 ﻿import os
 import re
 import copy
-data_dir = "E:/CurAct/data/Notebooks/DATA/KB/HTMLS"
-#data_dir = "E:/who/datathon/sunnystronghold/data/"
-def ParseHtmlFiles():
+def ParseHtmlFiles(data_dir='HTMLS/',max_num_files=0):
     f = 1
     data_samples = []
     for filename in os.listdir(data_dir) :
         if filename.endswith(".html") : 
             #print(f,filename)
-            html = open(data_dir + '\\' + filename,'r').read()
+            html = open(data_dir + '/' + filename,'r').read()
         
             html = re.sub(r'<.*?>', '\n', html)        
             html = re.sub(r'\n{1,}', '\n', html)
@@ -20,16 +18,9 @@ def ParseHtmlFiles():
         
             html = html[(ind+len(Cuttof_Keyword)):len(html)]
             data_samples.append(html)
-    ##        ind = html.find('Feedback')
-    ##        html = html[0:ind]
-        
-#            print(html)
             
             f = f + 1
-#            if f == 1000:
-#                break
-    print len(data_samples)
+            if (not max_num_files == 0) and f > max_num_files:
+                break
+    print('Processed',len(data_samples),'items.')
     return data_samples
-
-#dt = ParseHtmlFiles()
-#print dt
