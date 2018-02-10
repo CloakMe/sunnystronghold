2a6976cedc4244f9c340af982c32f706f04062b8
Evgeniy Marinov <marinov.evgeniy@gmail.com>
2018-02-10 20:05:20 +0200

    Added functionality to cut number of json docs.

--- load_parsed_htmls.py
+++ load_parsed_htmls.py
@@ -1,9 +1,13 @@
 import os
 import json
 
-def load_parsed_htmls(dir_path):
+def load_parsed_htmls(dir_path, 
+                      number_to_load=None):    
     res = []
-    for file_name in os.listdir(dir_path):
+    listed_files = os.listdir(dir_path)
+    for nmb, file_name in enumerate(listed_files):
+        if number_to_load is not None and nmb >= number_to_load:
+            break            
         file_path = os.path.join(dir_path, file_name)
         with open(file_path) as f:
             holder = json.load(f)
