c567d0b5238b1540f0959e40f187b1e7f2699d52
Milan Rusev <15890495+MilanRusev@users.noreply.github.com>
2018-02-10 17:26:28 +0200

    preprocess docs

--- load_parsed_htmls.py
+++ load_parsed_htmls.py
@@ -5,6 +5,7 @@ def load_parsed_htmls(dir_path):
     res = []
     for file_name in os.listdir(dir_path):
         file_path = os.path.join(dir_path, file_name)
-        holder = json.load(open(file_path))
-        res.append(holder)
+        with open(file_path) as f:
+            holder = json.load(f)
+            res.append(holder)
     return res
--- preprocess_docs.py
+++ preprocess_docs.py
@@ -3,5 +3,25 @@ def preprocess_docs(holders):
     return list(map(preprocess_doc, holders))
 
 def preprocess_doc(holder):
-    # TODO: do better stuff
-    return holder["Document"]
+    return preprocess_doc_assign_weights(holder)
+
+# TODO: list all distinct paragraph keys - maybe we've missed something
+def preprocess_doc_assign_weights(holder):
+    text = ""
+    
+    text = append_content(text, holder.get("Title"), 3)
+    text = append_content(text, holder.get("Keywords"), 3)
+    text = append_content(text, holder.get("Product"), 1)
+    text = append_content(text, holder.get("Purpose"), 1)
+    text = append_content(text, holder.get("Details"), 1)
+
+    return text
+
+def append_content(text, content, count):
+    if(content is not None):
+        if(type(content) is list):
+            content = " ".join(content)
+        content_mult = (content + " ")*count
+        return text + " " + content_mult
+    else:
+        return text
