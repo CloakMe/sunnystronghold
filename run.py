ed5426a59098f9da08ccde54ee82856a519908bb
Boyko Perfanov <b_perfanov@yahoo.com>
2018-02-10 10:56:34 -0500

    Update run.py to use the new parsing and preprocess method.

--- run.py
+++ run.py
@@ -9,21 +9,30 @@
 #http://scikit-learn.org/0.18/auto_examples/applications/topics_extraction_with_nmf_lda.html
 
 from __future__ import print_function
+import os
+import sys
 from time import time
 #import parse
+import load_parsed_htmls
 import preprocess_docs
 import lemmatization
-import preprocess
 
 from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
 from sklearn.decomposition import NMF, LatentDirichletAllocation
 from sklearn.datasets import fetch_20newsgroups
 
+dir_path = 'HTML_parsed01/'
 n_samples = 20 #0 for all
 n_features = 1000
 n_components = 10
 n_top_words = 20
-parsed_htmls = load_parsed_htmls(dir_path)
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print('No command-line arguments, assume',dir_path,'for dir path of parsed htmls into jsons.')
+    else:
+        dir_path = sys.argv[1]
+    
+parsed_htmls = load_parsed_htmls.load_parsed_htmls(dir_path)
 stringData = preprocess_docs.preprocess_docs(parsed_htmls)
 
 lemmaToken = lemmatization.LemmaTokenizer()
